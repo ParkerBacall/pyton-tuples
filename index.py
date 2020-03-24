f9196ef76ab42119e3a21ef4b85efd46e5dfee42
ParkerBacall <pbacall2@gmail.com>
2020-03-23 20:00:16 -0600

    updated notes

---
+++ index.py
@@ -0,0 +1,18 @@
+size = (100, 200)
+width = size[0]
+
+len(size) #2
+max(size) #200
+min(size) #100
+100 in size #true
+
+movement = [5, -2, -3, 4, -1]
+first_movement = movement[0] # 5
+movement[0] = 7
+movement.append(-5) #[7, -2, -3, 4, -1, -5]
+movement.remove(-3) #[7, -2, 4, -1, -5]
+
+starting_positions = {'p_0': 50, 'p_1': 100, 'p_2': 150}
+starting_positions['p_0'] #50
+starting_positions.keys()
+starting_positions.values())
